b94edc1f431739bbdcc65d3a2a343473872c3f0c
Roman Koshevoiy <koshevoiy777@gmail.com>
2026-08-10 10:06:49 +0300

    Publish Android version 1.0.1

--- config/mobile.php
+++ config/mobile.php
@@ -10,7 +10,7 @@ return [
     ],
     'updates' => [
         'android' => [
-            'latest_version' => env('MOBILE_ANDROID_LATEST_VERSION', '1.0.0'),
+            'latest_version' => env('MOBILE_ANDROID_LATEST_VERSION', '1.0.1'),
             'minimum_version' => env('MOBILE_ANDROID_MINIMUM_VERSION', '1.0.0'),
             'update_url' => env('MOBILE_ANDROID_UPDATE_URL', 'https://omc.pl.ua/download/android'),
         ],
